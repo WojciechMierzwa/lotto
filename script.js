f776a84160687532c8fe8e70771396aad75f3c02
WojciechMierzwa <96776516+WojciechMierzwa@users.noreply.github.com>
2023-02-14 12:58:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,20 @@ let jsonData = '{"drawings":['+
 '{"date": "02-02-2023","numbers": [18,29,31,35,40,46]},'+
 '{"date": "31-01-2023","numbers": [5,8,19,28,41,46]},'+
 '{"date": "28-01-2023","numbers": [4,24,34,39,40,44]},'+
-'{"date": "26-01-2023","numbers": [7,15,26,28,33,35]}]}';
+'{"date": "26-01-2023","numbers": [7,15,26,28,33,35]},'+
+'{"date": "24-01-2023","numbers": [3,6,8,10,13,33]},'+
+'{"date": "21-01-2023","numbers": [14,26,37,41,43,47]},'+
+'{"date": "19-01-2023","numbers": [1,2,18,22,41,48]},'+
+'{"date": "17-01-2023","numbers": [8,11,18,22,38,49]},'+
+'{"date": "14-01-2023","numbers": [14,23,34,39,44,45]},'+
+'{"date": "12-01-2023","numbers": [25,31,32,33,44,45]},'+
+'{"date": "10-01-2023","numbers": [3,10,11,30,40,48]},'+
+'{"date": "07-01-2023","numbers": [3,9,12,21,29,43]},'+
+'{"date": "05-01-2023","numbers": [7,21,31,43,45,49]},'+
+'{"date": "03-01-2023","numbers": [8,11,14,37,38,48]},'+
+'{"date": "31-12-2022","numbers": [16,19,29,36,46,47]},'+
+'{"date": "29-12-2022","numbers": [4,15,23,27,30,41]},'+
+'{"date": "27-01-2023","numbers": [2,9,11,17,28,41]}]}';
 
 
 let jsObject = JSON.parse(jsonData);
@@ -15,14 +28,20 @@ const numbers = Array(49).fill(0);
 
 var text1="<table><tr><th>Losowanie z dnia</th><th colspan=\"6\">Wytypowane Numery</th>";
 var ball1,ball2,ball3,ball4,ball5,ball6;
+
+for(let i=0; i<jsObject.drawings.length; i++)
+{
+  numbers[jsObject.drawings[i].numbers[0]-1]++;
+  numbers[jsObject.drawings[i].numbers[1]-1]++;
+  numbers[jsObject.drawings[i].numbers[2]-1]++;
+  numbers[jsObject.drawings[i].numbers[3]-1]++;
+  numbers[jsObject.drawings[i].numbers[4]-1]++;
+  numbers[jsObject.drawings[i].numbers[5]-1]++;
+
+}
+
 for(let i=0; i<7; i++)
     {
-        numbers[jsObject.drawings[i].numbers[0]-1]++;
-        numbers[jsObject.drawings[i].numbers[1]-1]++;
-        numbers[jsObject.drawings[i].numbers[2]-1]++;
-        numbers[jsObject.drawings[i].numbers[3]-1]++;
-        numbers[jsObject.drawings[i].numbers[4]-1]++;
-        numbers[jsObject.drawings[i].numbers[5]-1]++;
         ball1="<td><div class=\"ball\"><p class=\"number\">"+jsObject.drawings[i].numbers[0]+"</div></p></td>"
         ball2="<td><div class=\"ball\"><p class=\"number\">"+jsObject.drawings[i].numbers[1]+"</div></p></td>"
         ball3="<td><div class=\"ball\"><p class=\"number\">"+jsObject.drawings[i].numbers[2]+"</div></p></td>"
@@ -37,6 +56,7 @@ for(let i=0; i<7; i++)
     text1=text1+"</table>"
     document.getElementById("stats").innerHTML =
      text1;
+     
 
 
      var text2="<table><tr><th>Numer</th><th>Ilość powtórzeń</th><th>Numer</th><th>Ilość powtórzeń</th><th>Numer</th><th>Ilość powtórzeń</th><th>Numer</th><th>Ilość powtórzeń</th><th>Numer</th><th>Ilość powtórzeń</th>"
@@ -68,12 +88,76 @@ for(let i=0; i<7; i++)
      numbers[45], numbers[46], numbers[47], numbers[48]
      ];
 
-     const rainbowColors = [];
+     /*const rainbowColors = []; //problem z konwertowaniem hsl do rgb, kolory monochromatyczne zamiast teczy
         for (let i = 0; i < 49; i++) {
         let angle = i * 360 / 49;
         let color = `hsl(${angle}, 100%, 50%)`;
         rainbowColors.push(color);
-        }
+        }*/
+
+        const rainbowColors = [
+          "rgb(255, 0, 0)",
+          "rgb(255, 127, 0)",
+          "rgb(255, 255, 0)",
+          "rgb(127, 255, 0)",
+          "rgb(0, 255, 0)",
+          "rgb(0, 255, 127)",
+          "rgb(0, 255, 255)",
+          "rgb(0, 127, 255)",
+          "rgb(0, 0, 255)",
+          "rgb(127, 0, 255)",
+          "rgb(255, 0, 255)",
+          "rgb(255, 0, 127)",
+          "rgb(139, 0, 0)",
+          "rgb(139, 69, 0)",
+          "rgb(160, 82, 45)",
+          "rgb(205, 133, 63)",
+          "rgb(184, 134, 11)",
+          "rgb(218, 165, 32)",
+          "rgb(238, 232, 170)",
+          "rgb(189, 183, 107)",
+          "rgb(154, 205, 50)",
+          "rgb(85, 107, 47)",
+          "rgb(107, 142, 35)",
+          "rgb(124, 252, 0)",
+          "rgb(173, 255, 47)",
+          "rgb(0, 128, 0)",
+          "rgb(64, 224, 208)",
+          "rgb(0, 255, 255)",
+          "rgb(0, 255, 127)",
+          "rgb(0, 128, 128)",
+          "rgb(72, 209, 204)",
+          "rgb(0, 0, 128)",
+          "rgb(30, 144, 255)",
+          "rgb(70, 130, 180)",
+          "rgb(128, 0, 128)",
+          "rgb(139, 0, 139)",
+          "rgb(139, 69, 19)",
+          "rgb(139, 69, 19)",
+          "rgb(165, 42, 42)",
+          "rgb(178, 34, 34)",
+          "rgb(220, 20, 60)",
+          "rgb(255, 105, 180)",
+          "rgb(255, 192, 203)",
+          "rgb(255, 182, 193)",
+          "rgb(255, 0, 255)",
+          "rgb(199, 21, 133)",
+          "rgb(218, 112, 214)",
+          "rgb(186, 85, 211)",
+          "rgb(148, 0, 211)",
+          "rgb(128, 0, 128)",
+          "rgb(139, 0, 139)",
+          "rgb(128, 0, 0)"
+        ];
+        
+        
+        
+
+
+        
+
+        
+
      var barColors = [
         rainbowColors[0], rainbowColors[1], rainbowColors[2], rainbowColors[3], rainbowColors[4], rainbowColors[5], 
         rainbowColors[6], rainbowColors[7], rainbowColors[8], rainbowColors[9], rainbowColors[10], rainbowColors[11], 
@@ -107,19 +191,15 @@ for(let i=0; i<7; i++)
        }
      });
 
-     //generowany przez chat nie dziala poprawnie
-     let sortedNumbers = numbers.slice().sort((a, b) => b - a); // posortuj liczby w kolejności malejącej
-     let sixLargestNumbers = sortedNumbers.slice(0, 6); // pobierz pierwsze 6 największych liczb
      
-     let indices = [];
-     for (let i = 0; i < numbers.length; i++) {
-       if (sixLargestNumbers.includes(numbers[i]) && !indices.includes(i)) {
-         indices.push(i);
-       }
-       if (indices.length === 6) {
-         break;
-       }
-     }
+
+     const sortedIndexes = numbers
+     .map((number, index) => [number, index])
+     .sort((a, b) => b[0] - a[0])
+     .map(pair => pair[1])
+     .slice(0, 6);
+     
+     indices=sortedIndexes;
 
 
         
